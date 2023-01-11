59d3d0a17a4cc854d272e4d9aa313a8d4ee7ea65
chenaoh <jgm.ayala@yavirac.edu.ec>
2023-01-10 23:46:38 -0500

    first commit

---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient ) {
+    
+   }
+
+  ngOnInit(): void {
+    //this.getProducts();
+    this.getproduct();
+
+  }
+
+  getProducts(){
+    const response = this.httpClient.get('http://api.escuelajs.co/api/v1/products').subscribe
+    (response =>{ console.log(response);
+    });
+  }
+  getproduct(){
+    const url = 'http://api.escuelajs.co/api/v1/products/20';
+    const response = this.httpClient.get(url).subscribe
+    (response => {console.log(response);
+    });
+  }
+  createProduct(){
+    const data={
+      title : 'Libros',
+      price: 15,
+      description: 'utiles escolares /Jessica Ayala',
+      categoryId: 1,
+    }
+
+
+    const url = 'http://api.escuelajs.co/api/v1/products';
+     this.httpClient.post(url, data).subscribe
+    (response => {console.log(response);
+    });
+  }
+
+}
